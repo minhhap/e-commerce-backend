7325006a89538e9d8e5024ce9437f84d17a0a91b
minhhap <69835497+minhhap@users.noreply.github.com>
2021-09-18 20:47:01 -0700

    init Category and Product

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
+// Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Sets up the Express app to handle data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
