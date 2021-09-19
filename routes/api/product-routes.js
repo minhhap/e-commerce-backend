6cad38551243c04d2dd21fba84fa73183e9015ef
minhhap <69835497+minhhap@users.noreply.github.com>
2021-09-18 21:55:00 -0700

    fixed associations and added get all product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,24 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    // be sure to include its associated Category and Tag data
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+    .then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
@@ -14,6 +31,9 @@ router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
+
+
+
 
 // create new product
 router.post('/', (req, res) => {
